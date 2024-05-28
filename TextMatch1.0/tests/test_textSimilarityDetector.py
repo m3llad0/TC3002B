ff81ac2a5f97f57c2e0bd5f368b5a698facd1326
m3llad0 <a01655451@tec.mx>
2024-05-27 19:54:01 -0600

    Text Similarity Detector testing
    Test the check similarity function using the test dataset

---
+++ TextMatch1.0/tests/test_textSimilarityDetector.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+from textSimilarityDetector import TextSimilarityDetector
+from textPreprocessor import TextPreprocessor
+
+class TestTextSimilarityDetector(unittest.TestCase):
+    def setUp(self):
+        self.detector = TextSimilarityDetector('dataset/files')
+        self.test_directory = 'dataset/test_files'
+
+    def test_plagiarized_texts(self):
+        
+        for filename in os.listdir(self.test_directory):
+            if filename.endswith('.txt'):
+                file_path = os.path.join(self.test_directory, filename)
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    test_text = file.read()
+                    print(f"Results for {filename}:")
+                    self.detector.check_similarity(test_text, similarity_threshold=0.3)

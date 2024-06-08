701eb203890e3edb478b38a17c336af210cbaf18
m3llad0 <a01655451@tec.mx>
2024-06-07 20:02:37 -0600

    Upadted routes.py
    It now uses the new plagarismDetector class to check for plagarism in the text and return the result to the user

--- TextMatch2.0/backend/app/routes.py
+++ TextMatch2.0/backend/app/routes.py
@@ -1,6 +1,7 @@
 from flask_cors import CORS
 from app import app
 from flask import request, jsonify
+from app.model.plagarsimDetector import PlagiarismDetector
 
 CORS(app)
 
@@ -9,10 +10,18 @@ def plagarsim():
     try:
         data = request.get_json()
 
-        print(data['text'])
+        user_text = data['text']
 
-        # Logic to check for plagarsim
+        detector = PlagiarismDetector()
 
-        return jsonify({"message": "Hello World"}), 200
+        detector.set_user_input(user_text)
+
+        results = detector.get_results()
+
+        if results:
+            return jsonify({"plgarised_text": True, "results": results}), 200
+        else:
+            return jsonify({"plgarised_text": False, "results": "No hay plagio"} ), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
